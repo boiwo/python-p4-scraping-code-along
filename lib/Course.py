41b5b0c2a162fdc2afee926f2525ce85c4f4d8d4
benard <benardboiwo703@gmail.com>
2024-10-10 11:44:24 +0300

    initial commit

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,18 @@
+# class Course:
+#     pass
 class Course:
-    pass
+    def __init__(self, title, schedule, description):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __repr__(self):
+        return f"Course(title={self.title}, schedule={self.schedule}, description={self.description})"
+class Course:
+    def __init__(self, title="", schedule="", description=""):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __repr__(self):
+        return f"Course(title={self.title}, schedule={self.schedule}, description={self.description})"
